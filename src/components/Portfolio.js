730344d4fac0ea9c5bc7d0d057d813769624af79
quocthinh <quocthinh861@gmail.com>
2021-05-23 10:27:33 +0700

    first commit

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,10 +11,18 @@ export default function Portfolio(){
                 <div className='portfolio__container container swiper-container'>
                     <div className='swiper-wrapper'>
                         <div className='portfolio__content grid swiper-slide'>
+                            <img src='assets/images/netflix.png' className='portfolio__img'></img>
+                            <div className='portfolio__data'>
+                                <h3 className='portfolio__title'>Netflix Clone</h3>
+                                <p className='portfolio__description'>React App using firebase (firestore, Auth), styled components and hooks</p>
+                                <a href="https://quocthinh861.github.io/netflix-clone/" className='button button__flex button__small portfolio__button'>Demo <i class="icofont-caret-right button__icon"></i></a>
+                            </div>
+                        </div>
+                        <div className='portfolio__content grid swiper-slide'>
                             <img src='assets/images/movies.png' className='portfolio__img'></img>
                             <div className='portfolio__data'>
                                 <h3 className='portfolio__title'>Movies Website</h3>
-                                <p className='portfolio__description'>Movies Website using Hooks, Dbmovies API and Material UI</p>
+                                <p className='portfolio__description'>React Movies Website using Hooks, Dbmovies API and Material UI</p>
                                 <a href="https://quocthinh861.github.io/movies-react/" className='button button__flex button__small portfolio__button'>Demo <i class="icofont-caret-right button__icon"></i></a>
                             </div>
                         </div>
@@ -24,14 +32,6 @@ export default function Portfolio(){
                                 <h3 className='portfolio__title'>Pizza Banner</h3>
                                 <p className='portfolio__description'>Simple React using styled components and hooks</p>
                                 <a href="https://quocthinh861.github.io/pizza-banner/" className='button button__flex button__small portfolio__button'>Demo <i class="icofont-caret-right button__icon"></i></a>
-                            </div>
-                        </div>
-                        <div className='portfolio__content grid swiper-slide'>
-                            <img src='assets/images/netflix.png' className='portfolio__img'></img>
-                            <div className='portfolio__data'>
-                                <h3 className='portfolio__title'>Netflix Clone</h3>
-                                <p className='portfolio__description'>React App using firebase (firestore, Auth), styled components and hooks</p>
-                                <a href="https://quocthinh861.github.io/netflix-clone/" className='button button__flex button__small portfolio__button'>Demo <i class="icofont-caret-right button__icon"></i></a>
                             </div>
                         </div>
                     </div>
